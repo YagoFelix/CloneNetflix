cfb78cd89fc4793e6e76e03f9ad41364287aff21
Yago <y.felixdasilva@gmail.com>
2025-09-06 07:45:48 -0300

    feat: install dependences movie-trailer and react player from npm

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import "./Nav.css";
-
 function Nav() {
+  const [show, setShow] = React.useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setShow(window.scrollY > 100);
+    });
+  }, []);
+
   return (
-    <div>
-      Nav
+    <div className={`nav-container ${show && "nav-container-black"}`}>
+      <img
+        className="nav-logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix"
+      ></img>
+      <img
+        className="nav-avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
+        alt="Yago"
+      ></img>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
