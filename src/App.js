884fa4945f272423264eee31358114af2c4ac4b9
Yago <y.felixdasilva@gmail.com>
2025-09-06 07:23:27 -0300

    feat: add component Banner and iniciate component Nav

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import Row from './components/Row';
 import categories from './api';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      {/*Navbar*/}
-      {/*destaque*/}
-      {/*Em alta*/}
+      <Nav />
+      <Banner />
       {categories.map((category) => {
         return (
           <Row 
             key={category.name}
             title={category.title}
             path={category.path}
+            isLarge={category.isLarge}
           />
         )
       })}
